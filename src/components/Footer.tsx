dc52418cb7b896dfda428f7a85ea67bfaadfdc2e
zhugezifang <xiaoqiu2017wy@163.com>
2024-11-28 13:10:25 +0000

    feat:ko-fi

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer({
                                  description = ''
                                }) {
   return (
-    <footer className="bg-[#276ef1]" aria-labelledby="footer-heading">
+    <footer className="bg-indigo-50" aria-labelledby="footer-heading">
       <div id="footer-heading" className="sr-only">
         Footer
       </div>
@@ -27,24 +27,23 @@ export default function Footer({
                 className="h-10"
                 src="/favicon.svg"
                 width={32}
-                height={32}
-                alt="Perfect Tidy"
+                height={32}               
               />
             </a>
-            <p className="text-sm text-white">
+            <p className="text-sm ">
               {description}
             </p>
           </div>
           <div className="mt-2 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
             <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
-                <div className="text-sm font-semibold leading-6 text-white"></div>
+                <div className="text-sm font-semibold leading-6 "></div>
                 <ul role="list" className="mt-6 space-y-4">
 
                 </ul>
               </div>
               <div className="mt-10 md:mt-0">
-                <div className="text-sm font-semibold leading-6 text-white">Friends</div>
+                <div className="text-sm font-semibold leading-6 ">Friends</div>
                 <ul role="list" className="mt-6 space-y-4">
                         
                 </ul>
@@ -52,14 +51,14 @@ export default function Footer({
             </div>
             <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
-                <div className="text-sm font-semibold leading-6 text-white">Product</div>
+                <div className="text-sm font-semibold leading-6 ">Product</div>
                 <ul role="list" className="mt-6 space-y-4">
                   {navigation.product.map((item) => {
                       return (
                         <li key={item.name}>
                           <Link href={`${item.href}`}
                                 target={"_blank"}
-                                className="text-sm leading-6 text-white ">
+                                className="text-sm leading-6  ">
                             {item.name}
                           </Link>
                         </li>
@@ -69,7 +68,7 @@ export default function Footer({
                 </ul>
               </div>
               <div className="mt-10 md:mt-0">
-                <div className="text-sm font-semibold leading-6 text-white">Legal</div>
+                <div className="text-sm font-semibold leading-6 ">Legal</div>
                 <ul role="list" className="mt-6 space-y-4">
                   {navigation.legal.map((item) => {
                       let hrefTo = `${item.href}`;
@@ -79,7 +78,7 @@ export default function Footer({
                       return (
                         <li key={item.name}>
                           <Link href={`${hrefTo}`}
-                                className="text-sm leading-6 text-white ">
+                                className="text-sm leading-6  ">
                             {item.name}
                           </Link>
                         </li>
@@ -95,14 +94,14 @@ export default function Footer({
 
       <div className="mx-auto my-6 w-full border border-white lg:my-6"></div>
       <div>
-          <p className="font-inter lg: text-center text-sm text-white py-8">
+          <p className="font-inter lg: text-center text-sm  py-8">
             © Copyright 2024.{" "}
             <a
               href="/"
               target="_blank"
-              className="text-white font-bold"
+              className=" font-bold"
             >
-              Perfect Tidy
+              Combinations Game
             </a>{" "}
             All rights reserved.
           </p>
