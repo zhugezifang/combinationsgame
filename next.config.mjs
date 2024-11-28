8f6b6e23e36fa0296b88bba3ce90b164fd7d325a
zhugezifang <xiaoqiu2017wy@163.com>
2024-11-28 02:23:37 +0000

    feat:async_hooks fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          async_hooks: false,
+        };
+      }
+      return config;
+    },
+  };
 
 export default withNextIntl(nextConfig);
