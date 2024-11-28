93e97b692d12a148b51363c46317bf33df7c3eed
zhugezifang <xiaoqiu2017wy@163.com>
2024-11-28 02:51:06 +0000

    feat:next

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    distDir: 'dist', // 自定义构建目录
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
