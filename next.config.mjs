021b524eddcd66fed283e7ee4caab595e1c9f048
zhugezifang <xiaoqiu2017wy@163.com>
2024-11-28 02:38:19 +0000

    feat:next

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          ...config.resolve.fallback,
-          async_hooks: false,
-        };
-      }
-      return config;
-    },
-  };
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
